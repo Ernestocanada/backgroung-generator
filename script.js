6f650ff6d01af6a96bd4da9148a42bc66b037e97
Ernestocanada <ernesto.industrial@gmail.com>
2021-08-03 17:30:32 +0200

    Testing1

--- script.js
+++ script.js
@@ -3,17 +3,18 @@ const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
 
+color1.addEventListener("input", function () {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-color1.addEventListener("input",function() {
-    body.style.background = "linear-gradient(to right, "  + color1.value + ", " + color2.value + ")"; 
+  css.textContent = body.style.background + ";";
+});
 
-    css.textContent = body.style.background + ";";
-})
+color2.addEventListener("input", function () {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-color2.addEventListener("input",function() {
-    body.style.background = "linear-gradient(to right, "  + color1.value + ", " + color2.value + ")";
+  css.textContent = body.style.background + ";";
+});
 
-    css.textContent = body.style.background + ";";
-})
-
-
+console.log("Welcome");
